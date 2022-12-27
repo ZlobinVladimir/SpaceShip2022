f2d4aa44663533b326923243d904f9162ae84688
vladimir <delvin110@gmail.com>
2022-12-27 21:09:27 +0600

    tests

---
+++ README.md
@@ -0,0 +1,1 @@
+SpaceShip 2022
--- SpaceBattle.Lib.Test/TestRotate.cs
+++ SpaceBattle.Lib.Test/TestRotate.cs
@@ -2,7 +2,7 @@ namespace SpaceBattle.Lib.Test;
 public class TestRotate
 {
     [Fact]
-    public void Degree_135()
+    public void SuccessRotate()
     {
         Mock<IRotatable> rotateMock = new Mock<IRotatable>();
         rotateMock.Setup(a => a.angle).Returns(new Angle(45));
@@ -10,13 +10,34 @@ public class TestRotate
         var command = new RotateCommand(rotateMock.Object);
         command.Execute();
         rotateMock.VerifySet(a => a.angle = new Angle(135), Times.Once);   
-    } 
+    }
     [Fact]
-    public void No_Angle()
+    public void AngleGetException()
     {
         Mock<IRotatable> rotateMock = new Mock<IRotatable>();
-        rotateMock.Setup(a => a.angle).Returns(new Angle(45));
+        rotateMock.SetupGet<Angle>(m => m.angularVelocity).Returns(new Angle (90));
+        rotateMock.SetupSet<Angle>(m => m.angle = new Angle (135));
+        rotateMock.SetupGet<Angle>(m => m.angle).Throws<Exception>();
         var command = new RotateCommand(rotateMock.Object);
-
+        Assert.Throws<Exception>(() => command.Execute());
+    }
+    [Fact]
+    public void AngularVelGetException()
+    {
+        Mock<IRotatable> rotateMock = new Mock<IRotatable>();
+        rotateMock.SetupProperty<Angle>(m => m.angle, new Angle (45));
+        rotateMock.SetupGet<Angle>(m => m.angularVelocity).Throws<Exception>();
+        var command = new RotateCommand(rotateMock.Object);
+        Assert.Throws<Exception>(() => command.Execute());
+    }
+    [Fact]
+    public void AngleSetException()
+    {
+        Mock<IRotatable> rotateMock = new Mock<IRotatable>();
+        rotateMock.SetupProperty(m => m.angle, new Angle(45));
+        rotateMock.SetupGet<Angle>(m => m.angularVelocity).Returns(new Angle (90));
+        rotateMock.SetupSet<Angle>(m => m.angle = It.IsAny<Angle>()).Throws<Exception>();
+        var command= new RotateCommand(rotateMock.Object);
+        Assert.Throws<Exception>(() => command.Execute());
     }
 }
