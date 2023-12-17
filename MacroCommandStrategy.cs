b2fdfe54f9849537acb7a3d86bba98c415f08284
Злобин Владимир <115130839+ZlobinVladimir@users.noreply.github.com>
2023-12-18 00:02:18 +0600

    create MacroCommandStrategy

---
+++ MacroCommandStrategy.cs
@@ -0,0 +1,18 @@
+using Hwdtech;
+namespace SpaceBattle.Lib;
+public class MacroCommandStrategy: IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var obj1 = (IUObject)args[0];
+        var name = (string)args[1];
+        IEnumerable<string> NamesOfCommand = IoC.Resolve<IEnumerable<string>>("Config.MacroCommand." + name);
+        IEnumerable<ICommand> commands = new List<ICommand>();
+        var inameofcommand = NamesOfCommand.GetEnumerator();
+        while (inameofcommand.MoveNext())
+        {
+            commands.Append(IoC.Resolve<ICommand>(inameofcommand.Current, obj1));
+        }
+        return IoC.Resolve<ICommand>("SimpleMacroCommand", commands);
+    }
+}
