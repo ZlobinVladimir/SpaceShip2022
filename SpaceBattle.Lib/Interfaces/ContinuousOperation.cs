eb3244465233bbdf7a6319ec6615c35b6225c7b6
Семён Ливинский <ghurcb5@gmail.com>
2023-12-20 19:26:16 +0600

    Cleanup

--- SpaceBattle.Lib/Interfaces/ContinuousOperation.cs
+++ SpaceBattle.Lib/Interfaces/ContinuousOperation.cs
@@ -1,10 +1,8 @@
 using Hwdtech;
 namespace SpaceBattle.Lib;
 
-public class ContinuousOperation : IStrategy
-{
-    public object Run(params object[] args)
-    {
+public class ContinuousOperation : IStrategy{
+    public object Run(params object[] args){
         IUObject obj = (IUObject)args[1];
         var name = (string)args[0];
         var queue = IoC.Resolve<Queue<ICommand>>("Game.Queue", obj);
